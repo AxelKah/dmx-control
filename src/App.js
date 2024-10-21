03bfbae0bb5b1b42379ed9b0e38e2943ebccb371
nikoprl <45226198+nikoprl@users.noreply.github.com>
2024-10-21 18:31:44 +0300

    Add help page view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./views/Main";
+import HelpPage from "./views/Help";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/help" element={<HelpPage />} />
       </Routes>
     </Router>
   );
