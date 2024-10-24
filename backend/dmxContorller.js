7fe2e191147e51cc2615ee9d93e25e4b23aa12f7
AxelKah <axel.kahkonen@gmail.com>
2024-10-24 17:41:45 +0300

    added console.log

--- backend/dmxContorller.js
+++ backend/dmxContorller.js
@@ -164,6 +164,7 @@ app.post('/set-lights', (req, res) => {
 
     lights.forEach(light => {
         const { id, color } = light;
+        console.log(`Setting light ${id} to color ${color}`);
 
         // Map the light ID to its DMX channel
         const dmxChannel = id;
