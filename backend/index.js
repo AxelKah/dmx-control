4cbefd6b90cebdf8d9ba99f17ec98f68c3a05b1f
nikoprl <45226198+nikoprl@users.noreply.github.com>
2024-11-11 20:44:56 +0200

    Add mongo

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const dmxRoutes = require("./routes/dmxRouter");
 const gptRoutes = require("./routes/gptRouter");
 
@@ -15,6 +16,21 @@ app.use(express.json());
 // Use routes
 app.use("/", dmxRoutes);
 app.use("/gpt", gptRoutes);
+
+const mongoString = process.env.DB_URL;
+if (mongoString) {
+  mongoose.connect(mongoString);
+  mongoose.set("sanitizeFilter", true);
+
+  const database = mongoose.connection;
+  database.once("connected", () => {
+    console.log("Database Connected");
+  });
+
+  database.on("error", (error) => {
+    console.log(error);
+  });
+}
 
 // Start the app
 app.listen(port, () => {
