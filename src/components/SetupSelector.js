a3a69ee4468f085c46033fad822c5ad87c7a1b40
nikoprl <45226198+nikoprl@users.noreply.github.com>
2024-11-18 22:49:30 +0200

    Update styling

--- src/components/SetupSelector.js
+++ src/components/SetupSelector.js
@@ -85,39 +85,44 @@ const LightSetupSelector = ({ lights, setLights, setNumLights }) => {
   };
 
   return (
-    <div className="flex justify-center items-center">
-      <label htmlFor="lightsetups-dropdown">Saved light setups: </label>
-      <select
-        id="lights-dropdown"
-        value={selectedLightSetup || ""}
-        onChange={handleSetupChange}
-        className="ml-2 bg-white border border-white text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit p-2.5"
-      >
-        <option value="" disabled>
-          ...
-        </option>
-        {savedLights.map((light) => (
-          <option key={light.id} value={light.id}>
-            {light.name}
+    <div className="flex justify-center items-center w-full">
+      <div className="flex justify-center items-center flex-row bg-gray-100 p-2 rounded-lg shadow-lg m-2">
+        <label htmlFor="lightsetups-dropdown" className="font-bold text-sm">LIGHT SETUPS: </label>
+        <select
+          id="lights-dropdown"
+          value={selectedLightSetup || ""}
+          onChange={handleSetupChange}
+          className="ml-2 bg-gray-300 border border-white text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit p-2.5"
+        >
+          <option value="" disabled>
+            ...
           </option>
-        ))}
-      </select>
-      <button
-        onClick={handleUpdate}
-        className="ml-2 flex flex-row items-center"
-      >
-        <FaEdit className="mr-1" /> Update
-      </button>
-      <button
-        onClick={handleDelete}
-        className="ml-2 flex flex-row items-center"
-      >
-        <FaTrash className="mr-1" /> Delete
-      </button>
-      <button onClick={handleSave} className="ml-2 flex flex-row items-center">
-        <FaSave className="mr-1" />
-        Save
-      </button>
+          {savedLights.map((light) => (
+            <option key={light.id} value={light.id}>
+              {light.name}
+            </option>
+          ))}
+        </select>
+        <button
+          onClick={handleUpdate}
+          className="ml-2 flex flex-row items-center"
+        >
+          <FaEdit className="mr-1" /> Update
+        </button>
+        <button
+          onClick={handleDelete}
+          className="ml-2 flex flex-row items-center"
+        >
+          <FaTrash className="mr-1" /> Delete
+        </button>
+        <button
+          onClick={handleSave}
+          className="ml-2 flex flex-row items-center"
+        >
+          <FaSave className="mr-1" />
+          Save
+        </button>
+      </div>
     </div>
   );
 };
