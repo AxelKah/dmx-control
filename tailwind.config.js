2ead912b31d39a7e91dca920b7e1d596779e9e0a
nikoprl <45226198+nikoprl@users.noreply.github.com>
2024-11-29 17:48:33 +0200

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         "top-md":
           "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
       },
+      fontFamily: {
+        "arial": ["Arial, sans-serif"],
+     },
     },
   },
   plugins: [],
